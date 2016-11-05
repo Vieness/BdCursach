654c57ef6ecb599cdce2305cb872785934e24819
franchuz <ydvion13@gmail.com>
2016-11-05 13:12:40 +0200

    05/11/16
    new interface

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,14 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
+import java.awt.*;
+
 public class Controller {
+
+    public void buttonV(ActionEvent actionEvent) {
+
+    }
 }
